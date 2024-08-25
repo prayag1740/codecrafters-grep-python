e6dbfa6a2dfe8fcdbf21a169ab7f3523c6096164
prayag1740 <prayag.bhatia2015@vit.ac.in>
2024-08-25 08:52:19 -0700

    positive char groups

--- app/main.py
+++ app/main.py
@@ -16,6 +16,13 @@ def match_for_integer(input_line):
 def match_for_alphanumeric(input_line):
     return any(char.isdigit() or char.isalpha() for char in input_line)
 
+def match_for_positive_char_groups(input_line, pattern):
+    pattern_map = {p : True for p in pattern}
+    for ip in input_line:
+        if pattern_map.get(ip):
+            return True
+    return False
+
 
 def main():
     pattern = sys.argv[2]
@@ -28,6 +35,13 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     
+    if pattern[0] == "[" and pattern[-1] == "]":
+        stripped_pattern = pattern[1:-1]
+        if match_for_positive_char_groups(input_line, stripped_pattern):
+            exit(0)
+        else:
+            exit(1)
+    
     if pattern == "\\d":
         if match_for_integer(input_line):
             exit(0)
@@ -39,7 +53,7 @@ def main():
             exit(0)
         else:
             exit(1)
-
+            
     # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
